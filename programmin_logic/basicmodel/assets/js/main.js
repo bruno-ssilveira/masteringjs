919ee1fbfcb2f876e707f01939a8290138433b55
Bruno S Silveira <bruno.santossilveira001@gmail.com>
2025-05-19 13:55:08 -0300

    imc calculator finished

--- programmin_logic/basicmodel/assets/js/main.js
+++ programmin_logic/basicmodel/assets/js/main.js
@@ -1,2 +1,45 @@
 const peso = document.querySelector('#peso');
 const altura = document.querySelector('#altura');
+const form = document.querySelector('.form');
+const resultado = document.querySelector('.resultado');
+
+let imc;
+
+form.onsubmit = (event) => {
+	event.preventDefault();
+
+  	imc = parseFloat(peso.value) / (parseFloat(altura.value) * parseFloat(altura.value));
+	console.log(imc);
+
+	if(imc < 18.5) {
+		resultado.textContent = `Abaixo do peso. Seu IMC é ${imc} `;
+		resultado.style.background = '#f58723';
+		resultado.style.display = 'block';
+		resultado.style.color = '#FEFEFE';
+	} else if(imc < 24.9) {
+		resultado.textContent = `Peso normal. Seu IMC é ${imc} `;
+		resultado.style.background = '#93c2b1';
+		resultado.style.display = 'block';
+		resultado.style.color = '#FEFEFE';
+	} else if(imc < 29.9) {
+		resultado.textContent = `Sobrepeso. Seu IMC é ${imc} `;
+		resultado.style.background = '#2c6b74';
+		resultado.style.display = 'block';
+		resultado.style.color = '#FEFEFE';
+	} else if(imc < 34.9) {
+		resultado.textContent = `Obesidade grau 1. Seu IMC é ${imc} `;
+		resultado.style.background = '#63594d';
+		resultado.style.display = 'block';
+		resultado.style.color = '#FEFEFE';
+	} else if(imc < 39.9) {
+		resultado.textContent = `Obesidade grau 2. Seu IMC é ${imc} `;
+		resultado.style.background = '#3e3e53';
+		resultado.style.display = 'block';
+		resultado.style.color = '#FEFEFE';
+	} else if(imc >= 40) {
+		resultado.textContent = `Obesidade grau 3. Seu IMC é ${imc} `;
+		resultado.style.background = '#000000';
+		resultado.style.display = 'block';
+		resultado.style.color = '#FEFEFE';
+	}
+}
