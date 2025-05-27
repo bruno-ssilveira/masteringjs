7c8c123c2e83de52d5de1064a80600332b20b4cc
Bruno S Silveira <bruno.santossilveira001@gmail.com>
2025-05-27 13:17:44 -0300

    timer finished

--- programmin_logic/basicmodel/assets/js/timer.js
+++ programmin_logic/basicmodel/assets/js/timer.js
@@ -3,11 +3,35 @@ const pause = document.querySelector('.pause');
 const reset = document.querySelector('.reset');
 const time = document.querySelector('.time');
 
-let go = false;
-const date = new Date();
+let seconds = 0;
+let timer;
 
-play.addEventListener('click', () => {
-    console.log('play')
-    let dateTime = date.getTime();
-    time.innerText = dateTime;
+function createSeconds(seconds) {
+    date = new Date(seconds * 1000);
+    return date.toLocaleTimeString('pt-BR', {hour12: false, timeZone: 'UTC'});
+}
+
+function clockInit() {
+    timer = setInterval(function() {
+        seconds++;
+        time.innerText = createSeconds(seconds);
+    }, 1000);
+}
+
+play.addEventListener('click', function(event){
+    clearInterval(timer);
+    time.style.color = 'black';
+    clockInit();
 });
+
+pause.addEventListener('click', () => {
+    clearInterval(timer);
+    time.style.color = 'red';
+});
+
+reset.addEventListener('click', () => {
+    clearInterval(timer);
+    seconds = 0;
+    time.innerText = createSeconds(seconds);
+    time.style.color = 'black';
+})
