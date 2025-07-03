ab159ef9a41b7dc3d0b02b881e29e3fcbfce490d
Bruno S Silveira <bruno.santossilveira001@gmail.com>
2025-07-03 13:41:09 -0300

    polimorfism finished

--- objects_prototype/polimorfism.js
+++ objects_prototype/polimorfism.js
@@ -1,25 +1,68 @@
-// Superclass
+function round(value) {
+	return parseFloat(value.toFixed(2));
+}
+
+// SUPERCLASS - ACCOUNT
 function Account(branch, accountNumber, balance) {
 	this.branch = branch;
 	this.accountNumber = accountNumber;
-	this.balance = balance;
+	this.balance = round(balance);
 }
 
-Account.prototype.balance = function() {console.log(this.balance)}
-
-Account.prototype.withdraw = function(withdraw) {
-	if(this.balance < withdraw) return console.log(`Saldo insuficiente: R$${this.balance}`);
-	this.balance -= withdraw;
+Account.prototype.showBalance = function() {
+	console.log(`Branch/Acc: ${this.branch}/${this.accountNumber} | Balance: R$${this.balance.toFixed(2)}`);
 }
 
-Account.prototype.deposit = function(deposit) {
-	this.balance += deposit;
+Account.prototype.withdraw = function(value) {
+	if(this.balance < value) return console.log(`Insufficient funds: R$${this.balance.toFixed(2)}`);
+	this.balance -= value;
 }
 
-const account1 = new Account(1234, 121314, 100);
+Account.prototype.deposit = function(value) {
+	this.balance += value;
+}
 
-account1.balance();
+const account1 = new Account(1234, 121314, 100.20);
+
+// CHECKING ACCOUNT
+function CheckingAccount(branch, accountNumber, balance, limit) {
+	Account.call(this, branch, accountNumber, balance);
+	this.limit = limit;
+}
+
+CheckingAccount.prototype = Object.create(Account.prototype);
+
+CheckingAccount.prototype.withdraw = function(value) {
+	if(value > (this.balance + this.limit)) return console.log(`Insufficient funds: R$${this.balance.toFixed(2)}`);
+	this.balance -= value;
+}
+
+// SAVINGS ACCOUNT
+function SavingsAccount(branch, accountNumber, balance) {
+	Account.call(this, branch, accountNumber, balance);
+}
+SavingsAccount.prototype = Object.create(Account.prototype);
+
+console.log('Checking Account');
+const checkingAccount1 = new CheckingAccount(12, 121, 100, 99);
+checkingAccount1.showBalance();
+checkingAccount1.withdraw(99.01);
+checkingAccount1.showBalance();
+checkingAccount1.withdraw(90);
+checkingAccount1.showBalance();
+
+console.log();
+
+console.log('Savings Account')
+const savingsAccount1 = new SavingsAccount(12, 121, 100);
+savingsAccount1.showBalance();
+savingsAccount1.withdraw(99.01);
+savingsAccount1.showBalance();
+savingsAccount1.withdraw(90);
+savingsAccount1.showBalance();
+
+/*account1.showBalance();
 account1.withdraw(99);
-account1.balance();
+account1.showBalance();
 account1.deposit(100);
-account1.balance();
+account1.showBalance();*/
